c72c7bcd048a2581318620fe6027b561e55abaee
Matthias Geier <Matthias.Geier@gmail.com>
2022-12-11 14:15:51 +0100

    ssr 0.6.0 (new formula)
    Create a SSR formula
    
    Closes #1.
    
    Signed-off-by: Matthias Geier <705404+mgeier@users.noreply.github.com>

---
+++ Formula/ssr.rb
@@ -0,0 +1,48 @@
+class Ssr < Formula
+  desc "SoundScape Renderer: A tool for real-time spatial audio reproduction"
+  homepage "http://spatialaudio.net/ssr/"
+  url "https://github.com/SoundScapeRenderer/ssr/releases/download/0.6.0/ssr-0.6.0.tar.gz"
+  sha256 "a7d48047e6bd884aa25ae9a96f97efa57aed26d65967ee5e56a13945f34d35e1"
+  license "GPL-3.0-or-later"
+
+  depends_on "pkg-config" => :build
+  depends_on "asio"
+  depends_on "ecasound"
+  depends_on "fftw"
+  depends_on "fmt"
+  depends_on "jack"
+  depends_on "libsndfile"
+  depends_on "qt@5"
+  depends_on "SoundScapeRenderer/ssr/libmysofa"
+  depends_on "vrpn"
+  depends_on "websocketpp"
+
+  on_linux do
+    depends_on "mesa-glu"
+  end
+
+  def install
+    ENV["CPPFLAGS"] = "-I#{HOMEBREW_PREFIX}/include/libecasoundc"
+    # Make sure all expected features are switched on:
+    features = %w[
+      --enable-browser-gui
+      --enable-ecasound
+      --enable-fudi-interface
+      --enable-gui
+      --disable-intersense
+      --enable-ip-interface
+      --enable-polhemus
+      --enable-razor
+      --enable-sofa
+      --enable-vrpn
+      --enable-websocket-interface
+    ]
+    system "./configure", *std_configure_args, "--disable-silent-rules", *features
+    system "make", "install"
+  end
+
+  test do
+    # Not a great test, but it's hard to actually run the SSR in this context
+    assert_match "Usage", shell_output("#{bin}/ssr-binaural --help")
+  end
+end
