89c772684ea6859a216fb351e9a23ed74e8ebc0e
Matthias Geier <Matthias.Geier@gmail.com>
2023-07-31 10:45:35 +0200

    use temporary tarball from CirleCI

--- Formula/ssr.rb
+++ Formula/ssr.rb
@@ -1,8 +1,8 @@
 class Ssr < Formula
   desc "SoundScape Renderer: A tool for real-time spatial audio reproduction"
   homepage "http://spatialaudio.net/ssr/"
-  url "https://github.com/SoundScapeRenderer/ssr/releases/download/0.6.1/ssr-0.6.1.tar.gz"
-  sha256 "392a13ecbf86f980be76a31884a83ab762e577c1c829cb5e744f708542bc5bdb"
+  url "https://output.circle-artifacts.com/output/job/24c2adaf-bd86-435a-8067-da4f0fc0216e/artifacts/0/ssr-0.5.0-314-g7ce5325.tar.gz"
+  sha256 "217fdd5564e3e6756c81891bc369da7e95df3f550f9d76732c5fbbbe15bb508b"
   license "GPL-3.0-or-later"
 
   bottle do
